86d8705731e1190d60e061f88f0054581b791aa1
Lucas <lucasfilgueiras@rocketmail.com>
2021-04-13 15:34:48 -0300

    Comecando a implementar a impressão do tabuleiro

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -3,15 +3,20 @@ namespace tabuleiro
 {
     class Tabuleiro
     {
-        public int Linhas { get; set }
-        public int Colunas { get; set }
-        private Peca[,] Pecas;
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] pecas;
 
         public Tabuleiro(int linhas, int colunas)
         {
             Linhas = linhas;
             Colunas = colunas;
-            Pecas = new Peca[linhas, colunas];
+            this.pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
